c5265a3ffa4610af41c0f6d6d39e6df6b9c53388
zakariaCHOUKRI <zc.zakariachoukri23@gmail.com>
2023-12-20 11:37:25 +0100

    Remove unused code

--- app/data_processing.py
+++ app/data_processing.py
@@ -2,15 +2,6 @@
 import subprocess
 import pandas as pd
 
-def run_slurm_command(command):
-    result = subprocess.run(command, capture_output=True, text=True)
-    return result.stdout.strip()
-
-def get_partition_info():
-    command = ['sinfo', '--format=%P|%C|%G']
-    partition_data = run_slurm_command(command)
-    return partition_data
-
 def process_partition_data(partition_data):
     columns = ['Partition', 'AvailableCPUs', 'AvailableGPUs']
     data = [line.split('|') for line in partition_data.split('\n')]
